a4d31e85763008218000007790cdb36c2d9a5d5d
Gyeongsik Kim <admin@gyungdal.cc>
2019-11-12 01:19:16 +0900

    typescript init add test code'

---
+++ index.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import { Client } from "discord.js";
+const client = new Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login(process.env.DISCORD_SECRET);
