4c80b579b233482c4df37389d2ffe58f0fb5b538
Gyeongsik Kim <admin@gyungdal.cc>
2020-01-19 04:13:15 +0900

    rm 명령어 추가
    라이브러리 버전업그레이드

---
+++ src/command/test/i2x.ts
@@ -0,0 +1,56 @@
+import { IBot, ICommand, IMessage, ICommandDescription, CommandType } from "../../struct/api";
+import { Message } from "discord.js";
+import { BotMessage } from "../../struct/message";
+export class I2X implements ICommand {
+    public readonly bot: IBot;
+    constructor(bot: IBot) {
+        this.bot = bot;
+    }
+
+    get help(): ICommandDescription {
+        return {
+            type: CommandType.TEST,
+            command: "i2x",
+            desc: '이미지 사이즈 조정'
+        }
+    }
+
+    isValid(msg: Message): boolean {
+        if (msg.mentions.users.has(this.bot.config.id)) {
+            const check = msg.content.split(' ')[1].toLowerCase()
+                 === this.help.command;
+            this.bot.logger.info(`${this.help.command} : ${check}`);
+            return check;
+        }
+        return false;
+    }
+
+    process(msg: Message): Promise<Boolean> {
+        const message = new BotMessage(msg);
+        message.setTitle(this.help.desc);
+        message.setDescription("Pong!");
+        return message.sendReply()
+            .then((resolve) => {
+                setTimeout(() => {
+                    //array인지 체크
+                    if (Array.isArray(resolve)) {
+                        const sendMessages: Message[] = resolve as Message[];
+                        sendMessages.forEach(value => {
+                            if (value.deletable) {
+                                value.delete();
+                            }
+                        });
+                    } else {
+                        const sendMessage: Message = resolve as Message;
+                        if (sendMessage.deletable) {
+                            sendMessage.delete();
+                        }
+                    }
+                    message.removeRecvMessage();
+                }, 1000 * 10);
+                return Promise.resolve(true);
+            }, (reject) => {
+                return Promise.reject(reject);
+            });
+    }
+}
---
+++ src/command/test/rm.ts
@@ -0,0 +1,42 @@
+import { IBot, ICommand, ICommandDescription, CommandType } from "../../struct/api";
+import { Message } from "discord.js";
+import { BotMessage } from "../../struct/message";
+export class Ping implements ICommand {
+    public readonly bot: IBot;
+    constructor(bot: IBot) {
+        this.bot = bot;
+    }
+
+    get help(): ICommandDescription {
+        return {
+            type: CommandType.TEST,
+            command: "rm",
+            desc: '메세지 제거'
+        }
+    }
+
+    isValid(msg: Message): boolean {
+        if (msg.mentions.users.has(this.bot.config.id)) {
+            const check = msg.content.split(' ')[1].toLowerCase()
+                === this.help.command;
+            this.bot.logger.info(`${this.help.command} : ${check}`);
+            return check;
+        }
+        return false;
+    }
+
+    async process(msg: Message): Promise<Boolean> {
+        const countParse = Number(msg.content.split(' ')[2]);
+        let count;
+        if (Number.isNaN(countParse)) {
+            count = 1;
+        } else {
+            count = countParse;
+        }
+        return msg.channel.fetchMessages({ limit: count })
+            .then(success => msg.channel.bulkDelete(success)
+                , reject => Promise.reject(reject))
+            .then(sucess => Promise.resolve(true)
+                , reject => Promise.reject(reject));
+    }
+}
