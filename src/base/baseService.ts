3b0d2947ab5c3142dd910984e6e0a0e6b89812ad
Gyeongsik Kim <admin@gyungdal.cc>
2020-04-19 06:02:35 +0900

    argv 인자 디버그

--- src/base/baseService.ts
+++ src/base/baseService.ts
@@ -8,7 +8,7 @@ export class BaseService implements IService {
     protected _name : string;
 
     constructor(bot : IBot){
-        this._argv = new Array();
+        this._argv = new Array<string>();
         this._name = "";
         this._priority = 999;
         this._bot = bot;
@@ -24,23 +24,29 @@ export class BaseService implements IService {
     public get name() : string { 
         return this._name;
     }
+
     public get argv() : Array<string> {
         return this._argv;
     }
 
     addOrSetArgv(arg: string): void {
         if(!this._argv.includes(arg)){
+            this.bot.logger.info(`argv insert : ${arg}`);
             this._argv.push(arg);
+            this.bot.logger.info(`argv : ${this._argv.join(", ")}`);
         }
+
     }
     isValid(msg : Message) : boolean {
         throw new Error("Method not implemented.");
     }
+
     removeArgv(arg: string): void {
         if(this._argv.includes(arg)){
             delete this._argv[this._argv.findIndex(value => value === arg)];
         }
     }
+
     execute(msg : Message) : Promise<ServiceExecuteResultType>{
         throw new Error("Method not implemented.");
     }
